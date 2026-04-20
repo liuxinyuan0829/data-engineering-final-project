392d92c59e82889402f6fd1b905d9d9aa875e317
Xinyuan Liu <liuxinyuan0829@gmail.com>
2026-04-20 21:09:07 +0000

    add more data models

--- dbt_dota/models/staging/stg_main_metadata.sql
+++ dbt_dota/models/staging/stg_main_metadata.sql
@@ -1,1 +1,24 @@
-select * from {{ source('raw', 'main_metadata') }}
+select 
+
+cast(version as integer) as version,
+
+cast(match_id as integer) as match_id,
+
+cast(leagueid as integer) as league_id,
+
+cast(start_date_time as timestamp) as start_datetime,
+
+cast(start_date_time as date ) as start_date,
+
+cast(duration as integer) as duration,
+
+cast(first_blood_time as integer) as first_blood_time,
+
+cast(radiant_win as STRING) as radiant_win,
+
+cast(radiant_score as integer) as radiant_score,
+
+cast(dire_score as integer) as dire_score
+
+
+ from {{ source('raw', 'main_metadata') }}
